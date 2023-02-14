a0b2ee546c69fc019d6e076183553b83d837d924
TianaNanta <andrianjakananta@gmail.com>
2023-02-14 23:08:38 +0300

    update test

--- tests/test_yudownloader.py
+++ tests/test_yudownloader.py
@@ -2,11 +2,11 @@
 
 from typer.testing import CliRunner
 
-from yudownloader import __app_name__, __version__, cli
+from yudownloader import __app_name__, __version__, main
 
 runner = CliRunner()
 
 def test_version():
-    result = runner.invoke(cli.app, ["--version"])
+    result = runner.invoke(main.app, ["--version"])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
