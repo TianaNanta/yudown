1caed056281e54651672f688147ed018ca711625
TianaNanta <andrianjakananta@gmail.com>
2023-02-16 06:33:49 +0300

    Update script

--- yudown/__init__.py
+++ yudown/__init__.py
@@ -6,7 +6,7 @@ from os.path import expanduser
 from tinydb import Query, TinyDB
 
 __app_name__ = "YuDown"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 home = expanduser("~")
 yudown_dir = home+"/YuDown"
